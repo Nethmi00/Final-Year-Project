0b5c896f29b91401996b4c83c9359391a81bdf89
Nethmi Pabasara <netmipabasara912@gmail.com>
2025-03-04 15:46:58 +0530

    Add header Folder

---
+++ header.php
@@ -0,0 +1,15 @@
+<section class="header">
+
+<a href= "home.php" class="logo">Wedding.</a>
+
+<nav class="navbar">
+    <a href="home.php">Home</a>
+    <a href="Budget Planner.php">Budget Planner</a>
+    <a href="About Us.php">About Us</a>
+    <a href="Our Packages.php">Our Packages</a>
+    <a href="Plan Your Wedding.php">Plan Your Wedding</a>
+    <a href="Contact Us.php">Contact Us</a>
+</nav>
+
+<div id="menu-btn" class="fas fa-bars"></div>
+</section>
